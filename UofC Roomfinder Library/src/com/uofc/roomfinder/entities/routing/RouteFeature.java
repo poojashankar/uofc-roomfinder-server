53d427ad2da9c744b17191db008d81ab5ea05e0d
benjamin.lautenschlaeger <benjamin.lautenschlaeger@gmail.com>
2012-04-08 03:59:20 +0000

    - outsourced the source code which is relevant to both projects

---
+++ UofC Roomfinder Library/src/com/uofc/roomfinder/entities/routing/RouteFeature.java
@@ -0,0 +1,29 @@
+package com.uofc.roomfinder.entities.routing;
+
+public class RouteFeature {
+	double length;
+	String text;
+	
+	//constructor
+	public RouteFeature(double length, String text) {
+		super();
+		this.length = length;
+		this.text = text;
+	}
+	
+	//getter&setter
+	public double getLength() {
+		return length;
+	}
+	public void setLength(double length) {
+		this.length = length;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	//TODO: geometry
+}
